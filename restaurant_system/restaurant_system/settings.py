01264976dbd9e9ed58be7452cc9edd115dbd7a2e
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-06-22 12:18:10 +0100

    working on the database to connect to th postgresql

--- restaurant_system/restaurant_system/settings.py
+++ restaurant_system/restaurant_system/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'restaurant_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_db',
+        'USER': 'restaurant_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
